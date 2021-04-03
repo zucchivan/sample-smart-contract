af25768375c0d0e52aa8d0738ec8ddc00f4a2280
zucchivan <zucchivan@gmail.com>
2021-04-03 09:32:36 -0300

    Adding Mocha/Chai dependencies + initial unit tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Token = artifacts.require("Token");
-const Exchange = artifacts.require("Exchange");
+const Token = artifacts.require('Token');
+const Exchange = artifacts.require('Exchange');
 
 module.exports = async function(deployer) {
   await deployer.deploy(Token);
