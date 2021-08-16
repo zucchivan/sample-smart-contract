e654dc15b81b817fd9e404084d59e333e5c677ef
zucchivan <zucchivan@gmail.com>
2021-08-15 22:05:36 -0300

    Adds logos + updates contract abis + updates front-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,106 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
 import './App.css';
+import Navbar from "./Navbar";
+import SampleToken from "../abis/SampleToken.json";
+import SampleSmartContract from "../abis/SampleSmartContract.json";
+import Main from './Main';
+
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+    console.log(window.web3);
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ ethBalance })
+    console.log(this.state.ethBalance)
+
+    // Load SampleToken
+    const networkId = await web3.eth.net.getId()
+    const tokenData = SampleToken.networks[networkId]
+    if(tokenData) {
+      const token = new web3.eth.Contract(SampleToken.abi, tokenData.address)
+      this.setState({ token })
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+      this.setState({ tokenBalance: tokenBalance.toString() })
+    } else {
+      window.alert('SampleToken contract not deployed to detected network.')
+    }
+
+    // Load SampleSmartContract
+    const sampleSmartContractData = SampleSmartContract.networks[networkId]
+    if(sampleSmartContractData) {
+      const sampleSmartContract = new web3.eth.Contract(SampleSmartContract.abi, sampleSmartContractData.address)
+      this.setState({ sampleSmartContract })
+    } else {
+      window.alert('SampleSmartContract contract not deployed to detected network.')
+    }
+
+    this.setState({ loading: false })
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  buyTokens = (etherAmount) => {
+    this.setState({ loading: true })
+    this.state.sampleSmartContract.methods.buyTokens().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      token: {},
+      sampleSmartContract: {},
+      ethBalance: '0',
+      tokenBalance: '0',
+      loading: true
+    }
+  }
+
   render() {
+    let content
+
+    if(this.state.loading) {
+      content = <p id="loader" className="text-center">Loading...</p>
+    } else {
+      content = <Main
+        ethBalance={this.state.ethBalance}
+        tokenBalance={this.state.tokenBalance}
+        buyTokens={this.buyTokens}
+      />
+    }
+
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                {content}
               </div>
             </main>
           </div>
